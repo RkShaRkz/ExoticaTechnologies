d04640b647d70281974cdbf1384de0afcd9223f8
RkShaRkz <darksidemetatron@gmail.com>
2024-04-20 13:40:30 +0200

    The shield is functional, the shield-hosting drone is no longer being shot down
    However, the constant 'ticking' sound and the floating numbers lead me to think that
    there is a consistent and constant collision between the shieldhost and the owner ship.
    
    Need to look into that, and the fact that the owner-ship seems to rotate quite a bit
    or faster than it normally does - however i'm not entirely sure about this part.
    
    The guardianshield drone was changed and had it's bounds reduced to a small 2x2 square.
    It is also using a new transparent square sprite, so it's no longer visible on the ownership.
    This could be looked into for further improvement.
    
    In any case, the collision channel needs to be looked into before further refinements are made.

--- src/exoticatechnologies/modifications/exotics/impl/GuardianShield.kt
+++ src/exoticatechnologies/modifications/exotics/impl/GuardianShield.kt
@@ -3,9 +3,13 @@ package exoticatechnologies.modifications.exotics.impl
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.campaign.CampaignFleetAPI
 import com.fs.starfarer.api.campaign.econ.MarketAPI
+import com.fs.starfarer.api.characters.PersonAPI
 import com.fs.starfarer.api.combat.*
+import com.fs.starfarer.api.combat.listeners.CombatListenerManagerAPI
 import com.fs.starfarer.api.fleet.FleetMemberAPI
 import com.fs.starfarer.api.fleet.FleetMemberType
+import com.fs.starfarer.api.graphics.SpriteAPI
+import com.fs.starfarer.api.loading.WeaponSlotAPI
 import com.fs.starfarer.api.mission.FleetSide
 import com.fs.starfarer.api.ui.TooltipMakerAPI
 import com.fs.starfarer.api.ui.UIComponentAPI
@@ -13,7 +17,6 @@ import com.fs.starfarer.api.util.IntervalUtil
 import exoticatechnologies.modifications.ShipModifications
 import exoticatechnologies.modifications.exotics.Exotic
 import exoticatechnologies.modifications.exotics.ExoticData
-import exoticatechnologies.util.GuardianShieldFormation
 import exoticatechnologies.util.StringUtils
 import exoticatechnologies.util.Utilities
 import org.apache.log4j.Logger
@@ -21,15 +24,20 @@ import org.json.JSONObject
 import org.lazywizard.lazylib.MathUtils
 import org.lazywizard.lazylib.combat.AIUtils
 import org.lazywizard.lazylib.combat.CombatUtils
+import org.lwjgl.util.vector.Vector2f
 import org.magiclib.subsystems.MagicSubsystemsManager
 import org.magiclib.subsystems.drones.MagicDroneSubsystem
 import java.awt.Color
+import java.util.*
 
 
 class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) {
     override var color = Color(0xD93636)
     private lateinit var originalShip: ShipAPI
     private lateinit var originalOwnersShield: ShieldAPI
+
+    private val logger: Logger = Logger.getLogger(GuardianShieldDrone::class.java)
+
     override fun canAfford(fleet: CampaignFleetAPI, market: MarketAPI?): Boolean {
         return Utilities.hasItem(fleet.cargo, ITEM)
     }
@@ -65,7 +73,6 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
         // Secondly, give the ship a new subsystem to summon a drone hosting the Guardian Shield that will follow the ship around
         val subsystem = GuardianShieldDrone(ship)
         subsystem.setDronesToSpawn(1)
-//        subsystem.formation = GuardianShieldFormation()
         MagicSubsystemsManager.addSubsystemToShip(ship, subsystem)
     }
 
@@ -85,25 +92,22 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
         var engine: CombatEngineAPI = Global.getCombatEngine()
         private var drone: ShipAPI? = null
         private val tracker = IntervalUtil(0.3f, 0.5f)
-
-        override fun init() {
-            super.init()
-
-        }
+        private val shieldController = ShieldController()
 
         override fun onActivate() {
             super.onActivate()
             if (drone == null) {
                 drone = spawnDrone()
+                drone?.let { it.shield.toggleOn() }
             } else {
                 drone?.let { it.shield.toggleOn() }
             }
         }
 
-        override fun onFinished() {
-            super.onFinished()
-            drone?.let { it.shield.toggleOff() }
-        }
+//        override fun onFinished() {
+//            super.onFinished()
+//            drone?.let { it.shield.toggleOff() }
+//        }
 
 
         override fun getBaseActiveDuration(): Float = 0f
@@ -112,18 +116,6 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
 
         override fun getDisplayText(): String = "Guardian Shield"
 
-        /*
-        override fun getDroneVariant(): String {
-            val hullID = "exotica_guardianshield"
-            val variantList = Global.getSettings().hullIdToVariantListMap.getList(hullID)
-            if (variantList.isEmpty()) {
-                Logger.getLogger((GuardianShield::class as Any).javaClass).error("No variants for ${hullID}")
-            } else {
-                Logger.getLogger((GuardianShield::class as Any).javaClass).error("Hull ID ${hullID} contains following variants: ${variantList}")
-            }
-            return variantList.first()
-        }
-         */
         override fun getDroneVariant(): String = "exotica_guardianshield_standard"
 
         override fun targetOnlyEnemies(): Boolean = false
@@ -149,54 +141,7 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
                 it.facing = ship.facing
             }
 
-            tracker.advance(amount)
-            if (tracker.intervalElapsed()) {
-                val shipInDanger = ship.hullLevel < 0.15f
-                drone?.let {
-                    if (it.fluxTracker.fluxLevel > 0.96f && !shipInDanger) {
-                        systemOff()
-                        return false
-                    }
-                }
-                val allies = AIUtils.getNearbyAllies(ship, 1500f)
-                for (ally in allies) {
-                    if (ally.aiFlags != null
-                            && (ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.HAS_INCOMING_DAMAGE)
-                                    || ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))) {
-                        systemOn()
-                        return true
-                    }
-                }
-                val enemies = AIUtils.getNearbyEnemies(ship, 2000f)
-                for (enemy in enemies) {
-                    if ((enemy.aiFlags != null && enemy.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))
-                            || enemy.isRetreating) {
-                        continue
-                    }
-                    systemOn()
-                    return true
-                }
-                val projectiles: List<DamagingProjectileAPI> = engine.getProjectiles()
-                for (proj in projectiles) {
-                    if (proj.owner != ship.owner && MathUtils.getDistance(ship, proj) > 1500) {
-                        systemOn()
-                        return true
-                    }
-                }
-                val beams: List<BeamAPI> = engine.getBeams()
-                for (beam in beams) {
-                    if (beam.source.owner != ship.owner && MathUtils.isWithinRange(ship, beam.to, 1500f)) {
-                        systemOn()
-                        return true
-                    }
-                }
-                systemOff()
-                return false
-            }
-
-            // So, what do we do here? Lets fall back to "turn off shield"
-            systemOff()
-            return false
+            return shieldController.assesSituation(amount)
         }
 
         override fun advance(amount: Float, isPaused: Boolean) {
@@ -205,63 +150,25 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
             if (!isPaused) {
                 // Move the drone to center of ship
                 drone?.let {
-//                    it.fixedLocation = ship.location
-//                    it.facing = ship.facing
-//                    it.setCopyLocation(ship.location, ship.alphaMult, ship.facing)
                     it.location.set(ship.location.x, ship.location.y)
                     it.facing = ship.facing
                 }
 
-                tracker.advance(amount)
-                if (tracker.intervalElapsed()) {
-                    val shipInDanger = ship.hullLevel < 0.15f
-                    drone?.let {
-                        if (it.fluxTracker.fluxLevel > 0.96f && !shipInDanger) {
-                            systemOff()
-                        }
-                    }
-                    val allies = AIUtils.getNearbyAllies(ship, 1500f)
-                    for (ally in allies) {
-                        if (ally.aiFlags != null
-                                && (ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.HAS_INCOMING_DAMAGE)
-                                        || ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))) {
-                            systemOn()
-                        }
-                    }
-                    val enemies = AIUtils.getNearbyEnemies(ship, 2000f)
-                    for (enemy in enemies) {
-                        if ((enemy.aiFlags != null && enemy.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))
-                                || enemy.isRetreating) {
-                            continue
-                        }
-                        systemOn()
-                    }
-                    val projectiles: List<DamagingProjectileAPI> = engine.getProjectiles()
-                    for (proj in projectiles) {
-                        if (proj.owner != ship.owner && MathUtils.getDistance(ship, proj) > 1500) {
-                            systemOn()
-                        }
-                    }
-                    val beams: List<BeamAPI> = engine.getBeams()
-                    for (beam in beams) {
-                        if (beam.source.owner != ship.owner && MathUtils.isWithinRange(ship, beam.to, 1500f)) {
-                            systemOn()
-                        }
-                    }
-                    systemOff()
-                }
+                shieldController.assesSituation(amount)
             }
         }
 
         private fun systemOn() {
             drone?.let {
                 it.shield.toggleOn()
+                logger.info("systemOn()\tshield on ? ${it.shield.isOn}")
             }
         }
 
         private fun systemOff() {
             drone?.let {
                 it.shield.toggleOff()
+                logger.info("systemOff()\tshield off ? ${it.shield.isOff}")
             }
         }
 
@@ -283,7 +190,81 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
             Global.getCombatEngine().getFleetManager(ship.owner).isSuppressDeploymentMessages = false
 
             fighter.isForceHideFFOverlay = true
+            // Make the shield-hosting drone invulnerable, so that it's not targetable.
+            // Because the owning ship keeps shooting it down.
+            fighter.mutableStats.hullDamageTakenMult.modifyMult(INVULNERABLE_SHIELD_DRONE, 0f)
+            logger.info("<-- spawnDrone()\tdrone shield: ${fighter.shield}, shield arc: ${fighter.shield.arc}, shield type: ${fighter.shield.type}, shield active ? ${fighter.shield.isOn}")
             return fighter
+        }
+
+        inner class ShieldController {
+
+            fun assesSituation(amount: Float): Boolean {
+                tracker.advance(amount)
+                if (tracker.intervalElapsed()) {
+                    val shipInDanger = ship.hullLevel < 0.15f
+
+                    drone?.let {
+                        val shieldInDanger = it.hullLevel < 1f
+                        if (it.fluxTracker.fluxLevel > 0.96f && !shipInDanger && !shieldInDanger) {
+                            logger.info("calling systemOff()\t\tif (it.fluxTracker.fluxLevel > 0.96f && !shipInDanger && !shieldInDanger)")
+                            systemOff()
+                            return false
+                        }
+                    }
+                    val allies = AIUtils.getNearbyAllies(ship, 1500f)
+                    // add shield to allies if it's non-null
+                    if (drone != null) {
+                        allies.add(drone)
+                    }
+                    // check if anybody needs help
+                    for (ally in allies) {
+                        if (ally.aiFlags != null
+                                && (ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.HAS_INCOMING_DAMAGE)
+                                        || ally.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))) {
+                            logger.info("calling systemOn()\t\tallies need help condition")
+                            systemOn()
+                            return true
+                        }
+                    }
+                    val enemies = AIUtils.getNearbyEnemies(ship, 2000f)
+                    for (enemy in enemies) {
+                        if ((enemy.aiFlags != null && enemy.aiFlags.hasFlag(ShipwideAIFlags.AIFlags.NEEDS_HELP))
+                                || enemy.isRetreating) {
+                            continue
+                        }
+                        logger.info("calling systemOn()\t\tnearby enemies condition")
+                        systemOn()
+                        return true
+                    }
+                    val projectiles: List<DamagingProjectileAPI> = engine.getProjectiles()
+                    for (proj in projectiles) {
+                        if (proj.owner != ship.owner
+                                && (MathUtils.getDistance(ship, proj) > 1500 || MathUtils.getDistance(drone, proj) > 1500)) {
+                            logger.info("calling systemOn()\t\tprojectiles near ship or shield condition")
+                            systemOn()
+                            return true
+                        }
+                    }
+                    val beams: List<BeamAPI> = engine.getBeams()
+                    for (beam in beams) {
+                        if (beam.source.owner != ship.owner
+                                && (MathUtils.isWithinRange(ship, beam.to, 1500f)
+                                        || MathUtils.isWithinRange(drone, beam.to, 1500f))) {
+                            logger.info("calling systemOn()\t\tbeam within range of ship or shield condition")
+                            systemOn()
+                            return true
+                        }
+                    }
+                    logger.info("calling systemOff()\t\tno other conditions were met")
+                    systemOff()
+                    return false
+                }
+
+                // what do we do here? lets fallback to returning false, but don't touch the shield
+                logger.info("just returning false, not touching shield")
+                return false
+            }
         }
     }
 
@@ -291,6 +272,7 @@ class GuardianShield(key: String, settings: JSONObject) : Exotic(key, settings) 
     companion object {
         private const val ITEM = "et_ammospool"
 
+        private const val INVULNERABLE_SHIELD_DRONE = "invulnerable_shield_drone"
         private const val BUFF_ID = "guardian_shield_buff"
         private const val SHIELD_DAMAGE_TAKEN_MULTIPLIER = 2f
     }
