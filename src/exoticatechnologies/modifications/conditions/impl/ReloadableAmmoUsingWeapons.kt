cb4b47c2abff94a089f326e17d35401b7bb2abd5
RkShaRkz <darksidemetatron@gmail.com>
2024-07-30 02:29:59 +0200

    Stuff that might be useful later

--- src/exoticatechnologies/modifications/conditions/impl/ReloadableAmmoUsingWeapons.kt
+++ src/exoticatechnologies/modifications/conditions/impl/ReloadableAmmoUsingWeapons.kt
@@ -6,16 +6,37 @@ import com.fs.starfarer.api.fleet.FleetMemberAPI
 import com.fs.starfarer.api.impl.campaign.ids.Tags
 import exoticatechnologies.modifications.ShipModifications
 import exoticatechnologies.modifications.conditions.OperatorCondition
+import org.apache.log4j.Logger
 
 
 class ReloadableAmmoUsingWeapons : OperatorCondition() {
     override val key = "reloadableAmmoUsingWeapons"
 
     override fun getActual(member: FleetMemberAPI, mods: ShipModifications?, variant: ShipVariantAPI): Any? {
+        log("member: ${member}\tid: ${member.id}\tmember name: ${member.shipName}\thullID: ${member.hullId}")
+
+        val fittedWeaponSlots = variant.fittedWeaponSlots
+        val weaponIds = fittedWeaponSlots.map { slotId -> variant.getWeaponId(slotId) }
+        val weaponSpecAPIs = weaponIds.map { weaponId -> Global.getSettings().getWeaponSpec(weaponId) }
+        val reloadingAmmoUsingWeapons = weaponSpecAPIs.map { weaponSpecAPI ->
+            log("WeaponSpecAPI: ${weaponSpecAPI}\tuses ammo ? ${weaponSpecAPI.usesAmmo()}\thas NO_RELOAD tag ? ${weaponSpecAPI.hasTag(Tags.NO_RELOAD)}")
+            weaponSpecAPI.usesAmmo() && !weaponSpecAPI.hasTag(Tags.NO_RELOAD)
+        }
+        val count = reloadingAmmoUsingWeapons.count { it }
+        log("Counted ${count} weapons meeting criteria")
+
         return variant.fittedWeaponSlots
                 .map { slotId -> variant.getWeaponId(slotId) }
                 .map { weaponId -> Global.getSettings().getWeaponSpec(weaponId) }
-                .map { weaponSpecAPI -> weaponSpecAPI.usesAmmo() && !weaponSpecAPI.hasTag(Tags.NO_RELOAD)}
+                .map { weaponSpecAPI -> weaponSpecAPI.usesAmmo() && !weaponSpecAPI.hasTag(Tags.NO_RELOAD) }
                 .count { it }
+    }
+
+    private fun log(log: String) {
+        logger.info("[ReloadableAmmoUsingWeapons] $log")
+    }
+
+    companion object {
+        private val logger: Logger = Logger.getLogger(ReloadableAmmoUsingWeapons::class.java)
     }
 }
--- src/exoticatechnologies/util/KtxUtils.kt
+++ src/exoticatechnologies/util/KtxUtils.kt
@@ -2,6 +2,8 @@ package exoticatechnologies.util
 
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.combat.WeaponAPI
+import org.apache.log4j.Logger
 
 /**
  * Method that plays a sound with a [soundId] ID, using the [ship]'s location and velocity,
@@ -15,4 +17,39 @@ fun playSound(soundId: String, ship: ShipAPI, pitch: Float = 1.0f, volume: Float
             ship.location,
             ship.velocity
     )
+}
+
+/**
+ * For simple ships, this is exactly the same as [ShipAPI.getAllWeapons] but for multi-segmented ships, it will
+ * walk through all segments and return *all* weapons on the ship
+ */
+fun getAllShipWeapons(ship: ShipAPI): List<WeaponAPI> {
+    AnonymousLogger.log("--> getAllShipWeapons()\tship: ${ship}\tship hullspec hullname: ${ship.hullSpec.hullName}\tship childModulesCopy size: ${ship.childModulesCopy.size}\tis ship with modules: ${ship.isShipWithModules}")
+    val weaponList: MutableList<WeaponAPI> = mutableListOf()
+    weaponList.addAll(ship.allWeapons)
+    AnonymousLogger.log("getAllShipWeapons()\tship: ${ship}\tship's all weapons: ${ship.allWeapons.map { weapon -> weapon.id }}")
+    AnonymousLogger.log("getAllShipWeapons()\tship.variant.stationModules.size: ${ship.variant.stationModules.size}")
+    if (ship.childModulesCopy.isNotEmpty()) {
+        for (module in ship.childModulesCopy) {
+            AnonymousLogger.log("getAllShipWeapons()\tmodule: ${module}\tmodule name: ${module.name}\tmodule hullspec hullname: ${module.hullSpec.hullName}\tmodule's all weapons: ${module.allWeapons.map { weapon -> weapon.id }}")
+            weaponList.addAll(module.allWeapons)
+        }
+    }
+    AnonymousLogger.log("<-- getAllShipWeapons()\treturning ${weaponList.map {weapon -> weapon.id} }")
+
+
+
+    return weaponList
+}
+
+object AnonymousLogger {
+    private val logger: Logger = Logger.getLogger(AnonymousLogger::class.java)
+
+    fun log(log: String) {
+        logger.info("[AnonymousLogger] $log")
+    }
+
+    fun log(log: String, logtag: String) {
+        logger.info("[$logtag] $log")
+    }
 }
