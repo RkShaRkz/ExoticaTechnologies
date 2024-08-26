370b16ad111e768ad8b4b1bde4b37067f3daccd1
RkShaRkz <darksidemetatron@gmail.com>
2024-08-26 03:34:29 +0200

    TimeUtils javadoc

--- src/exoticatechnologies/util/timeutils/TimeUtils.kt
+++ src/exoticatechnologies/util/timeutils/TimeUtils.kt
@@ -4,6 +4,10 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import kotlin.math.floor
 
+/**
+ * Returns locale-dependant local time by calling [System.currentTimeMillis]
+ * and formatting it in a "HH:mm:ss" format, ex. 12:34:56
+ */
 fun getClockTime() : String {
     val currentMillis = System.currentTimeMillis()
     val currentTime = Date(currentMillis)
@@ -12,17 +16,25 @@ fun getClockTime() : String {
     return timeFormatter.format(currentTime)
 }
 
+/**
+ * If frames are being used anywhere, and the game counts 1 second
+ * as 60 frames - this method will return a formatted time extracted
+ * from those frames in a "HH:mm:ss" format.
+ *
+ * Internally calls [getTimeFromSeconds]
+ */
 fun getTimeFromFrames(frames: Float): String {
     // The system runs in constant 60FPS so 60 frames is equal to 1 second.
     // Having that in mind, we just need to divide frames by 60 to get the number of seconds
     val FRAMES_PER_SECOND = 60
-    val SECONDS_IN_MINUTE = 60
-    val SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE
 
     val baseSeconds = floor(frames / FRAMES_PER_SECOND)
     return getTimeFromSeconds(baseSeconds = baseSeconds)
 }
 
+/**
+ * Returns a "HH:mm:ss" format of duration represented by [baseSeconds]
+ */
 fun getTimeFromSeconds(baseSeconds: Float): String {
     val SECONDS_IN_MINUTE = 60
     val SECONDS_IN_HOUR = 60 * SECONDS_IN_MINUTE
