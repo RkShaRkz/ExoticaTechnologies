5ccf4792fd36d40d30fa0ec9372d170238dd2806
Shark <darksidemetatron@gmail.com>
2025-03-29 17:53:39 +0100

    CR comment - add logger to ExoticHullmodLookup
    And make it not log anything below WARN.
    The log below the "ugly part" is now ERROR so should be logged still.

--- src/exoticatechnologies/hullmods/exotics/ExoticHullmodLookup.kt
+++ src/exoticatechnologies/hullmods/exotics/ExoticHullmodLookup.kt
@@ -1,15 +1,19 @@
 package exoticatechnologies.hullmods.exotics
 
-import exoticatechnologies.util.AnonymousLogger
 import exoticatechnologies.util.datastructures.Optional
+import exoticatechnologies.util.shouldLog
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 
 object ExoticHullmodLookup {
+    private val logger: Logger = Logger.getLogger(ExoticHullmodLookup::class.java)
+    private val MIN_LOG_LEVEL: Level = Level.WARN
 
     private val lookupMap: HashMap<String, ExoticHullmod> = hashMapOf()
 
     fun addToLookupMap(exoticHullmod: ExoticHullmod) {
         lookupMap[exoticHullmod.hullModId] = exoticHullmod
-        AnonymousLogger.log("<-- addToLookupMap()\tadded ExoticHullmod: ${exoticHullmod} to lookup map with ID: ${exoticHullmod.hullModId}\t\tlookupMap: ${lookupMap}", "ExoticHullmodLookup")
+        logIfOverMinLogLevel("<-- addToLookupMap()\tadded ExoticHullmod: ${exoticHullmod} to lookup map with ID: ${exoticHullmod.hullModId}\t\tlookupMap: ${lookupMap}", Level.INFO)
     }
 
     fun getFromMap(hullmodId: String): Optional<ExoticHullmod> {
@@ -18,11 +22,20 @@ object ExoticHullmodLookup {
 //                Optional.of(it)
 //            }
 
-            // Ugh, lets do the ugly thing...
+            // Ugh, lets do the ugly thing... but should be fine since it's contained in the map.
             Optional.of(lookupMap[hullmodId]!!)
         } else {
-            AnonymousLogger.log("Looking up unknown key ${hullmodId}, returning NOTHING since it's not in the lookup map.", "ExoticHullmodLookup")
+            logIfOverMinLogLevel("Looking up unknown key ${hullmodId}, returning NOTHING since it's not in the lookup map.", Level.ERROR)
             Optional.empty()
         }
+    }
+
+    private fun logIfOverMinLogLevel(logMsg: String, logLevel: Level) {
+        shouldLog(
+                logMsg = logMsg,
+                logger = logger,
+                logLevel = logLevel,
+                minLogLevel = MIN_LOG_LEVEL
+        )
     }
 }
