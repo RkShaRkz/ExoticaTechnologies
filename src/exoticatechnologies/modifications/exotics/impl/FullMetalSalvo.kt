2fef592434c1813e0352ce00ae05c750145dcbaf
Shark <darksidemetatron@gmail.com>
2025-09-21 19:44:21 +0200

    Touch-up FullMetalSalvo
    While there was nothing inherently wrong with it, I did get confused about the whole
    "works on whole ship", expecting it to light up all modules at the same time.
    
    Turns out, each module activates on it's own when appropriate, thus we don't need
    to apply the AfterImage to the whole ship when that happens, just let each module light
    up when it activates and that's that.
    
    Additionally, the "bonus" and "malus" parts were extracted into separate methods, as well as
    making the "projectile speed" and "damage boost" bonuses take positive mult into their
    actual calculation instead of just using a constant for it. Same thing for "rate of fire" malus,
    which takes in the negative multiplier.
    
    That being said, the "positives" are:
    - projectile speed boost
    - damage boost
    - duration
    
    while cooldown and rate of fire maluses are "negatives". The coefficient is neither, and will remain a flat
    33% until further notice/revision because when something is applied to it, it quickly gets out of hand with
    e.g. Pure type, which will then apply the positive mult twice, resulting in a 6.25x increase (e.g. 206% instead of 33%)
    
    Visual flair on "improved bullets" (bullets coming from Metal Salvo-activated modules) was considered but might
    happen at a later point as well.

--- src/exoticatechnologies/modifications/exotics/impl/FullMetalSalvo.kt
+++ src/exoticatechnologies/modifications/exotics/impl/FullMetalSalvo.kt
@@ -31,13 +31,44 @@ class FullMetalSalvo(key: String, settings: JSONObject) : Exotic(key, settings) 
     ) {
         if (expand) {
             StringUtils.getTranslation(key, "longDescription")
-                    .format("projSpeedBoost", DAMAGE_BUFF)
-                    .format("damageBoost", DAMAGE_BUFF * 0.33f)
-                    .formatFloat("boostTime", BUFF_DURATION * getPositiveMult(member, mods, exoticData))
-                    .formatFloat("cooldown", COOLDOWN * getNegativeMult(member, mods, exoticData))
-                    .format("firerateMalus", abs(RATE_OF_FIRE_DEBUFF))
+                    .format("projSpeedBoost", getProjectileSpeedBoostBonus(member, mods, exoticData))
+                    .format("damageBoost", getDamageBoostBonus(member, mods, exoticData))
+                    .formatFloat("boostTime", getBoostTimeBonus(member, mods, exoticData))
+                    .formatFloat("cooldown", getCooldownMalus(member, mods, exoticData))
+                    .format("firerateMalus", abs(getRateOfFireMalus(member, mods, exoticData)))
                     .addToTooltip(tooltip, title)
         }
+    }
+
+    // Now the utility bonus/malus applying getters for constants, with helper methods for individual bonuses/penalties
+    fun getProjectileSpeedBoostBonus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return getDamageBuffBonus(member, mods, exoticData)
+    }
+
+    fun getDamageBoostBonus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return getDamageBuffBonus(member, mods, exoticData) * getDamageBoostCoeficientBonus(member, mods, exoticData)
+    }
+
+    fun getBoostTimeBonus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return BUFF_DURATION * getPositiveMult(member, mods, exoticData)
+    }
+
+    fun getCooldownMalus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return COOLDOWN * getNegativeMult(member, mods, exoticData)
+    }
+
+    fun getRateOfFireMalus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return RATE_OF_FIRE_DEBUFF * getNegativeMult(member, mods, exoticData)
+    }
+
+    fun getDamageBoostCoeficientBonus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        // We will not be applying the positive multiplier to the coeficient as well, because then it will ramp up the
+        // damage boost from 33% to 206% (due to the positive multiplier appearing twice, and 2.5*2.5=6.25) for Pure type
+        return DAMAGE_BUFF_COEF// * getPositiveMult(member, mods, exoticData)
+    }
+
+    fun getDamageBuffBonus(member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData): Float {
+        return DAMAGE_BUFF * getPositiveMult(member, mods, exoticData)
     }
 
     override fun applyExoticToStats(
@@ -47,14 +78,20 @@ class FullMetalSalvo(key: String, settings: JSONObject) : Exotic(key, settings) 
             mods: ShipModifications,
             exoticData: ExoticData
     ) {
-        stats.ballisticRoFMult.modifyMult(buffId, 1 + RATE_OF_FIRE_DEBUFF / 100f)
-        stats.energyRoFMult.modifyMult(buffId, 1 + RATE_OF_FIRE_DEBUFF / 100f)
+        // these will turn out to be 1+ -33/100 = 0.67 which makes the weapons slower
+        stats.ballisticRoFMult.modifyMult(buffId, 1 + getRateOfFireMalus(member, mods, exoticData) / 100f)
+        stats.energyRoFMult.modifyMult(buffId, 1 + getRateOfFireMalus(member, mods, exoticData) / 100f)
     }
 
-    fun gigaProjectiles(source: ShipAPI) {
+    fun gigaProjectiles(source: ShipAPI, member: FleetMemberAPI, mods: ShipModifications, exoticData: ExoticData) {
         for (proj in Global.getCombatEngine().projectiles) {
             if (proj.source == source && proj.elapsed <= Global.getCombatEngine().elapsedInLastFrame) {
-                proj.damage.modifier.modifyMult(buffId, 1 + DAMAGE_BUFF / 100f * 0.33f)
+                val damageBoostBonus = getDamageBuffBonus(member, mods, exoticData) / 100f
+                val damageBoostCoef = getDamageBoostCoeficientBonus(member, mods, exoticData)
+                // Don't be fooled, *everything* is always nullable in alex's starsector API ...
+                proj?.let { nonNullProj ->
+                    nonNullProj.damage.modifier.modifyMult(buffId, 1 + damageBoostBonus * damageBoostCoef)
+                }
             }
         }
     }
@@ -66,10 +103,6 @@ class FullMetalSalvo(key: String, settings: JSONObject) : Exotic(key, settings) 
             mods: ShipModifications,
             exoticData: ExoticData
     ) {
-//        if (ActivatorManager.getActivators(ship)?.filterIsInstance<SalvoActivator>()?.isEmpty() != false) {
-//            val activator = SalvoActivator(ship, member, mods, exoticData)
-//            ActivatorManager.addActivator(ship, activator)
-//        }
         if (MagicSubsystemsManager.getSubsystemsForShipCopy(ship)?.filterIsInstance<SalvoActivator>()?.isEmpty() != false) {
             val activator = SalvoActivator(ship, member, mods, exoticData)
             MagicSubsystemsManager.addSubsystemToShip(ship, activator)
@@ -90,24 +123,24 @@ class FullMetalSalvo(key: String, settings: JSONObject) : Exotic(key, settings) 
         }
 
         override fun getBaseActiveDuration(): Float {
-            return BUFF_DURATION * getPositiveMult(member, mods, exoticData)
+            return getBoostTimeBonus(member, mods, exoticData)
         }
 
         override fun getBaseCooldownDuration(): Float {
-            return COOLDOWN * getNegativeMult(member, mods, exoticData)
+            return getCooldownMalus(member, mods, exoticData)
         }
 
         override fun advance(amount: Float, isPaused: Boolean) {
             if (state == State.ACTIVE) {
-                gigaProjectiles(ship)
+                gigaProjectiles(ship, member, mods, exoticData)
             }
         }
 
         override fun onStateSwitched(oldState: State) {
             if (state == State.ACTIVE) {
-                ship.mutableStats.ballisticProjectileSpeedMult.modifyMult(this.toString(), 1 + DAMAGE_BUFF / 100f)
-                ship.mutableStats.energyProjectileSpeedMult.modifyMult(this.toString(), 1 + DAMAGE_BUFF / 100f)
-                ship.mutableStats.missileMaxSpeedBonus.modifyMult(this.toString(), 1 + DAMAGE_BUFF / 100f)
+                ship.mutableStats.ballisticProjectileSpeedMult.modifyMult(this.toString(), 1 + getDamageBuffBonus(member, mods, exoticData) / 100f)
+                ship.mutableStats.energyProjectileSpeedMult.modifyMult(this.toString(), 1 + getDamageBuffBonus(member, mods, exoticData) / 100f)
+                ship.mutableStats.missileMaxSpeedBonus.modifyMult(this.toString(), 1 + getDamageBuffBonus(member, mods, exoticData) / 100f)
 
                 ship.addAfterimage(
                         Color(255, 125, 0, 150),
@@ -158,6 +191,7 @@ class FullMetalSalvo(key: String, settings: JSONObject) : Exotic(key, settings) 
 
     companion object {
         private const val DAMAGE_BUFF = 100f
+        private const val DAMAGE_BUFF_COEF = 0.33f
         private const val RATE_OF_FIRE_DEBUFF = -33f
         private const val COOLDOWN = 8f
         private const val BUFF_DURATION = 2
