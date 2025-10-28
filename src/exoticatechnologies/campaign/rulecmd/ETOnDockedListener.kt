46b6eb648f70f8e537663b9acf5c5da6fefd09d7
Shark <darksidemetatron@gmail.com>
2025-10-28 21:25:14 +0100

    rules.csv triggering the OnDockedListener - part1
    This commit actually has all it needs, this works good. I just figured to separate it into pieces.
    
    This commit adds a small rules.csv modification to trigger the OnDockedListener when the player docks;
    from there we will run through all of the FleetMembers in the player's fleet, promote their FleetData to
    ETFleetData - which will have a onPreScuttle() and onPostScuttle() callbacks - and go back to life as usual.
    
    Then, when the ship is scuttled, we can "intercept" or react to that and give back all of it's chips to the player.
    
    **THEN** this whole thing needs to be behind a lunasetting switch to enable/disable it.

---
+++ src/exoticatechnologies/campaign/rulecmd/ETOnDockedListener.kt
@@ -0,0 +1,17 @@
+package exoticatechnologies.campaign.rulecmd
+
+import com.fs.starfarer.api.campaign.InteractionDialogAPI
+import com.fs.starfarer.api.campaign.rules.MemoryAPI
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin
+import com.fs.starfarer.api.util.Misc
+import exoticatechnologies.util.AnonymousLogger
+import exoticatechnologies.util.StarsectorAPIInteractor
+
+class ETOnDockedListener : BaseCommandPlugin() {
+    override fun execute(ruleId: String?, dialog: InteractionDialogAPI?, params: MutableList<Misc.Token>?, memoryMap: MutableMap<String, MemoryAPI>?): Boolean {
+        AnonymousLogger.log("ETOnDockedListener::execute() called, reacting to player landing!", "ETOnDockedListener")
+        StarsectorAPIInteractor.promoteFleetDatasOnPlayerShipsIfNotAlreadyPromoted()
+
+        return false
+    }
+}
