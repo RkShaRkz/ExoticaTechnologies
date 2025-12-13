53e9c4d2d6c45b3d0b2077b863f7ce04bcab0cc7
Shark <darksidemetatron@gmail.com>
2025-12-13 21:28:53 +0100

    Minor touchups, helga validated everything still works.
    Thats it, ship it.

--- src/exoticatechnologies/campaign/listeners/CampaignEventListener.kt
+++ src/exoticatechnologies/campaign/listeners/CampaignEventListener.kt
@@ -57,15 +57,15 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
             val allFleets: MutableList<CampaignFleetAPI> = mutableListOf()
             allFleets.addAll(battle.bothSides ?: listOf())
             allFleets
-                    .filterNot { it == Global.getSector().playerFleet }
-                    .forEach {
-                        if (activeFleets.contains(it)) {
-                            return
-                        }
-                        dlog("Generating modifications for fleet.")
-                        activeFleets.add(it)
-                        applyExtraSystemsToFleet(it)
+                .filterNot { it == Global.getSector().playerFleet }
+                .forEach {
+                    if (activeFleets.contains(it)) {
+                        return
                     }
+                    dlog("Generating modifications for fleet.")
+                    activeFleets.add(it)
+                    applyExtraSystemsToFleet(it)
+                }
 
             // Grab memory map, and fire if non-null, log warning if was null
             val memoryMap = getMemoryMap(dialog.plugin)
@@ -156,13 +156,13 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
         }
 
         if (Entities.DEBRIS_FIELD_SHARED == interactionTarget.customEntityType
-                && interactionTarget.memoryWithoutUpdate.contains(MemFlags.SALVAGE_SPECIAL_DATA)
-                && interactionTarget.memoryWithoutUpdate[MemFlags.SALVAGE_SPECIAL_DATA] is ShipRecoverySpecialData
+            && interactionTarget.memoryWithoutUpdate.contains(MemFlags.SALVAGE_SPECIAL_DATA)
+            && interactionTarget.memoryWithoutUpdate[MemFlags.SALVAGE_SPECIAL_DATA] is ShipRecoverySpecialData
         ) {
 
             val data = interactionTarget.memoryWithoutUpdate[MemFlags.SALVAGE_SPECIAL_DATA] as ShipRecoverySpecialData
             if (data.ships != null
-                    && data.ships.isNotEmpty()
+                && data.ships.isNotEmpty()
             ) {
 
                 val derelictVariantMap: MutableMap<String, ShipModifications> = LinkedHashMap()
@@ -205,9 +205,9 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
     }
 
     override fun reportFleetDespawned(
-            fleet: CampaignFleetAPI,
-            reason: FleetDespawnReason,
-            param: Any?
+        fleet: CampaignFleetAPI,
+        reason: FleetDespawnReason,
+        param: Any?
     ) {
         dlog(String.format("Fleet %s has despawned.", fleet.nameWithFaction))
         activeFleets.remove(fleet)
@@ -226,24 +226,24 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
                 mods.bandwidth *= (0.5f + 0.5f * ShipModFactory.random.nextFloat())
 
                 mods.exotics.exoticData
-                        .filter { (_, data) -> data.exotic.canDropFromCombat }
-                        .filter { (_, data) -> ShipModFactory.random.nextFloat() >= data.exotic.getSalvageChance(8f) }
-                        .forEach { (_, data) -> mods.removeExotic(data.exotic) }
+                    .filter { (_, data) -> data.exotic.canDropFromCombat }
+                    .filter { (_, data) -> ShipModFactory.random.nextFloat() >= data.exotic.getSalvageChance(8f) }
+                    .forEach { (_, data) -> mods.removeExotic(data.exotic) }
 
                 mods.getUpgradeMap()
-                        .forEach { (upg, level) ->
-                            val mult = (0.5f + 0.5f * ShipModFactory.random.nextFloat()) * (1 + upg.salvageChance)
+                    .forEach { (upg, level) ->
+                        val mult = (0.5f + 0.5f * ShipModFactory.random.nextFloat()) * (1 + upg.salvageChance)
 
-                            mods.putUpgrade(upg, (mult.coerceAtMost(1f) * level).roundToInt().coerceAtLeast(1))
-                        }
+                        mods.putUpgrade(upg, (mult.coerceAtMost(1f) * level).roundToInt().coerceAtLeast(1))
+                    }
             }
         }
 
         val potentialDrops = getDrops(result, npcMembers)
         result.battle.getPrimary(result.battle.nonPlayerSide).memoryWithoutUpdate.set(
-                "\$exotica_drops",
-                potentialDrops,
-                1f
+            "\$exotica_drops",
+            potentialDrops,
+            1f
         )
 
         val playerResult = if (result.didPlayerWin()) result.winnerResult else result.loserResult
@@ -357,15 +357,15 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
         private const val debug = false
         private val log = Logger.getLogger(CampaignEventListener::class.java)
         private val submarketIdsToCheckForSpecialItems: MutableList<String> =
-                mutableListOf(Submarkets.SUBMARKET_BLACK, Submarkets.SUBMARKET_OPEN, Submarkets.GENERIC_MILITARY)
+            mutableListOf(Submarkets.SUBMARKET_BLACK, Submarkets.SUBMARKET_OPEN, Submarkets.GENERIC_MILITARY)
         val activeFleets: MutableList<CampaignFleetAPI> = ArrayList()
             get() = ArrayList(field).also { it.add(Global.getSector().playerFleet) }
 
         var mergeCheck = false
 
         private fun getDrops(
-                result: EngagementResultAPI,
-                members: List<FleetMemberAPI>
+            result: EngagementResultAPI,
+            members: List<FleetMemberAPI>
         ): Pair<Map<String, MutableMap<Int, Int>>, Map<ExoticData, Int>> {
             val upgradesMap: MutableMap<String, MutableMap<Int, Int>> = HashMap()
             val exotics: MutableMap<ExoticData, Int> = HashMap()
@@ -423,27 +423,27 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
 
         private fun checkNearbyFleetsForFM(fmId: String): FleetMemberAPI? {
             return Global.getSector().currentLocation.fleets
-                    .map { getFromFleet(fmId, it.fleetData) }
-                    .firstOrNull()
+                .map { getFromFleet(fmId, it.fleetData) }
+                .firstOrNull()
         }
 
         private fun checkStorageMarketsForFM(fmId: String): FleetMemberAPI? {
             Global.getSector().allLocations
-                    .flatMap { it.allEntities }
-                    .filter { it.market != null }
-                    .flatMap { it.market.submarketsCopy }
-                    .map { it.cargoNullOk }
-                    .filterNotNull()
-                    .forEach { storage ->
-                        var fm = getFromFleet(fmId, storage.mothballedShips)
-                        if (fm != null) {
-                            return fm
-                        }
-                        fm = getFromFleet(fmId, storage.fleetData)
-                        if (fm != null) {
-                            return fm
-                        }
+                .flatMap { it.allEntities }
+                .filter { it.market != null }
+                .flatMap { it.market.submarketsCopy }
+                .map { it.cargoNullOk }
+                .filterNotNull()
+                .forEach { storage ->
+                    var fm = getFromFleet(fmId, storage.mothballedShips)
+                    if (fm != null) {
+                        return fm
                     }
+                    fm = getFromFleet(fmId, storage.fleetData)
+                    if (fm != null) {
+                        return fm
+                    }
+                }
             return null
         }
 
@@ -457,7 +457,7 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
                     continue
                 }
                 if (!fleet.isAlive
-                        || fleet.containingLocation != playerFleet.containingLocation
+                    || fleet.containingLocation != playerFleet.containingLocation
                 ) {
                     removedFleet = true
                 }
@@ -518,8 +518,8 @@ class CampaignEventListener(permaRegister: Boolean) : BaseCampaignEventListener(
         private fun isInFleet(fmId: String, fleet: CampaignFleetAPI?): Boolean {
             fleet?.let {
                 return isInFleet(
-                        fmId,
-                        fleet.fleetData
+                    fmId,
+                    fleet.fleetData
                 )
             }
             return false
