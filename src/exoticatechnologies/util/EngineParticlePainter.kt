268f829d79d782fa2fd84eb3e52f1feabcb03fea
Shark <darksidemetatron@gmail.com>
2025-12-20 21:19:58 +0100

    Extended EngineParticlePainter with spawnEmpArc() method and related classes

--- src/exoticatechnologies/util/EngineParticlePainter.kt
+++ src/exoticatechnologies/util/EngineParticlePainter.kt
@@ -1,6 +1,9 @@
 package exoticatechnologies.util
 
 import com.fs.starfarer.api.combat.CombatEngineAPI
+import com.fs.starfarer.api.combat.CombatEntityAPI
+import com.fs.starfarer.api.combat.DamageType
+import com.fs.starfarer.api.combat.ShipAPI
 import org.lwjgl.util.vector.Vector2f
 import java.awt.Color
 
@@ -8,8 +11,92 @@ import java.awt.Color
  * Wrapper class around `CombatEngineAPI.add<whatever>Particle` methods to decouple my code from directly invoking
  * StarSector API code and make it more testable. Also provides a single entry-point to all these various particle-drawing
  * methods via [addParticle]
+ *
+ * Is also a wrapper around `CombatEngineAPI.spawnEmpArc<whatever>` methods via a single entry-point to all these via [spawnEmpArc]
  */
 object EngineParticlePainter {
+
+    /**
+     * Method for spawning EMP arcs. Depending on the [arcType], a different kind of [EmpArcParams] has to be provided.
+     *
+     * @param engine the [CombatEngineAPI] instance to use for drawing EMP arcs
+     * @param arcType the [EmpArcType] to draw:
+     *
+     * - [EmpArcType.NORMAL] needs a [EmpArcParams.DamagingArc] class parameter instance
+     * - [EmpArcType.PIERCE_SHIELDS] needs a [EmpArcParams.DamagingArc] class parameter instance
+     * - [EmpArcType.VISUAL] needs a [EmpArcParams.VisualArc] class parameter instance
+     *
+     * @param arcParams the [EmpArcParams] containing data to use in drawing this EMP arc
+     *
+     * **NOTE:** be **VERY VERY WARNED** that spawning (many) EMP arcs is, for some reason, very heavy on the rendering pipeline,
+     * and as such using this method alot/frequently will tank the FPS. You *should* prefer using [addParticle] over this!
+     */
+    fun spawnEmpArc(
+        engine: CombatEngineAPI,
+        arcType: EmpArcType,
+        arcParams: EmpArcParams
+    ) {
+        when (arcType) {
+            EmpArcType.NORMAL -> {
+                when(arcParams) {
+                    is EmpArcParams.DamagingArc -> {
+                        engine.spawnEmpArc(
+                            arcParams.damageSource,
+                            arcParams.from,
+                            arcParams.fromAnchor,
+                            arcParams.targetEntity,
+                            arcParams.damageType,
+                            arcParams.damAmount,
+                            arcParams.empDamAmount,
+                            arcParams.maxRange,
+                            arcParams.impactSoundId,
+                            arcParams.thickness,
+                            arcParams.fringe,
+                            arcParams.core
+                        )
+                    }
+                    is EmpArcParams.VisualArc -> throw IllegalArgumentException("`arcParams` must be of EmpArcParams.DamagingArc type when using EmpArcType.NORMAL !!!")
+                }.exhaustive
+            }
+            EmpArcType.PIERCE_SHIELDS -> {
+                when(arcParams) {
+                    is EmpArcParams.DamagingArc -> {
+                        engine.spawnEmpArcPierceShields(
+                            arcParams.damageSource,
+                            arcParams.from,
+                            arcParams.fromAnchor,
+                            arcParams.targetEntity,
+                            arcParams.damageType,
+                            arcParams.damAmount,
+                            arcParams.empDamAmount,
+                            arcParams.maxRange,
+                            arcParams.impactSoundId,
+                            arcParams.thickness,
+                            arcParams.fringe,
+                            arcParams.core
+                        )
+                    }
+                    is EmpArcParams.VisualArc -> throw IllegalArgumentException("`arcParams` must be of EmpArcParams.DamagingArc type when using EmpArcType.PIERCE_SHIELDS !!!")
+                }.exhaustive
+            }
+            EmpArcType.VISUAL -> {
+                when(arcParams) {
+                    is EmpArcParams.DamagingArc -> throw IllegalArgumentException("`arcParams` must be of EmpArcParams.VisualArc type when using EmpArcType.VISUAL !!!")
+                    is EmpArcParams.VisualArc -> {
+                        engine.spawnEmpArcVisual(
+                            arcParams.from,
+                            arcParams.fromAnchor,
+                            arcParams.to,
+                            arcParams.toAnchor,
+                            arcParams.thickness,
+                            arcParams.fringe,
+                            arcParams.core
+                        )
+                    }
+                }.exhaustive
+            }
+        }.exhaustive
+    }
 
     /**
      * Method for adding particles. Depending on the [particleType], a different kind of [ParticleParams] has to be provided.
@@ -644,3 +731,67 @@ sealed class ParticleParams(
         }
     }
 }
+
+/**
+ * General upper-level sealed class of all shared parameters for all emp arcs
+ *
+ * @param from the [Vector2f] location where the emp arc should start
+ * @param fromAnchor the anchor to which [from] should be anchored (and relative to), if any
+ * @param thickness the thickness of the emp arc
+ * @param fringe the fringe color of the emp arc
+ * @param core the core color of the emp arc
+ *
+ * @see DamagingArc
+ * @see VisualArc
+ */
+sealed class EmpArcParams(
+    open val from: Vector2f,
+    open val fromAnchor: CombatEntityAPI?, // nullable per API docs
+    open val thickness: Float,
+    open val fringe: Color,
+    open val core: Color
+) {
+    /**
+     * Class describing the [CombatEngineAPI.spawnEmpArc] and [CombatEngineAPI.spawnEmpArcPierceShields] methods' parameters
+     *
+     * @param damageSource damage source of the EMP arc, if any
+     * @param targetEntity the target of this EMP arc
+     * @param damageType the [DamageType] of this EMP arc
+     * @param damAmount the damage amount of this EMP arc
+     * @param empDamAmount the EMP damage amount of this EMP arc
+     * @param maxRange the max range of this EMP arc
+     * @param impactSoundId the ID of the impact sound, if any
+     */
+    data class DamagingArc(
+        val damageSource: ShipAPI?,
+        override val from: Vector2f,
+        override val fromAnchor: CombatEntityAPI?,
+        val targetEntity: CombatEntityAPI,
+        val damageType: DamageType,
+        val damAmount: Float,
+        val empDamAmount: Float,
+        val maxRange: Float,
+        val impactSoundId: String?,
+        override val thickness: Float,
+        override val fringe: Color,
+        override val core: Color,
+    ) : EmpArcParams(from, fromAnchor, thickness, fringe, core)
+
+    /**
+     * Class describing the [CombatEngineAPI.spawnEmpArcVisual] method's parameters
+     *
+     * @param to the [Vector2f] location where the EMP arc should end
+     * @param toAnchor the anchor to which [to] should be anchored (and relative to), if any
+     */
+    data class VisualArc(
+        override val from: Vector2f,
+        override val fromAnchor: CombatEntityAPI?,
+        val to: Vector2f,
+        val toAnchor: CombatEntityAPI?,
+        override val thickness: Float,
+        override val fringe: Color,
+        override val core: Color,
+    ) : EmpArcParams(from, fromAnchor, thickness, fringe, core)
+}
+
+enum class EmpArcType { NORMAL, PIERCE_SHIELDS, VISUAL }
